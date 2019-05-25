08662ef57c864167c347738d1dfe989e8daac704
olusegun <segunekisola@yahoo.com>
2019-05-25 17:44:43 +0100

    Changed the HTML for the site.

--- picture/views.py
+++ picture/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse("hello again")
+def post_list(request):
+    return render(request, 'picture/post_list.html', {})
 
 # Create your views here.
