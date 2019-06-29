a4d6439226342a3a07ace5c82ad3c31df4e50ebc
olusegun <segunekisola@yahoo.com>
2019-06-29 01:23:04 +0100

    New entries

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from picture import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('picture.urls')),
+    path('back/', views.baking),
+
 
 ]
